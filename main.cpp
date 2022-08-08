1c3eeee9d87736c4fb4ade5b6a221777c420d44b
Nikhil <nikhilsaraswat2605@gmail.com>
2022-08-08 19:21:31 +0530

    commited

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define IOS ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define int long long
+typedef unsigned long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<string, string> pss;
+typedef vector<pair<int, int>> vpii;
+typedef vector<pair<ll, ll>> vpll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<pii> vii;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+#define fi first
+#define se second
+#define all(arr) arr.begin(),arr.end()
+#define MP make_pair
+#define pb push_back
+#define repeat(i,s,n) for(int i=s;i<n;i++)
+const int mod = 1e9+7;
+
+void solve(int arr[])
+{
+    arr[3] = 8;
+    return;
+}
+
+
+signed main()
+{
+
+    int arr[] = {1,2,3,4,5};
+    solve(arr);
+    for(int i=0;i<5;i++) cout << arr[i] << " ";
+    cout << endl;
+    return 0;
+}
