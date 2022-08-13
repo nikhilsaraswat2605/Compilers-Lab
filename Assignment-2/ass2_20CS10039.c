6f33c310e9a5f848bd314e2a4494eb045a290c87
Nikhil <nikhilsaraswat2605@gmail.com>
2022-08-13 09:53:37 +0530

    commited

--- Assignment-2/ass2_20CS10039.c
+++ Assignment-2/ass2_20CS10039.c
@@ -14,12 +14,34 @@ int printStr(char * str){
 }
 
 int readInt(int *n){
-    
+
 }
 
 
 int printInt(int n){
-
+    char buff[32];
+    int len=0,neg=0;
+    if(n<0){
+        buff[len++]='-';
+        n=-n;
+        neg=1;
+    }
+    while(n){
+        buff[len++]=n%10+'0';
+        n/=10;
+    }
+    int i=0;
+    if(neg)
+        i++;
+    while(i<len/2){
+        char temp=buff[i];
+        buff[i]=buff[len-i-1];
+        buff[len-i-1]=temp;
+        i++;
+    }
+    buff[len]='\0';
+    printStr(buff);
+    return len;
 }
 
 int readFlt(float *f){
@@ -27,7 +49,14 @@ int readFlt(float *f){
 }
 
 int printFlt(float f){
-
+    long long int int_part = (long long int)f;
+    long long int frac_part = (long long int)((f - (float)int_part)*1000000);
+    printInt(int_part);
+    printStr(".");
+    if(frac_part == 0)
+        printStr("0");
+    else
+        printInt(frac_part);
 }
 
 
--- Assignment-2/main.c
+++ Assignment-2/main.c
@@ -1,6 +1,10 @@
 #include "myl.h"
 
 int main(){
-    char str = "Hello World";
+    char str[] = "Hello World\n\n";
     printStr(str);
+    printInt(432);
+    printStr("\n\n");
+    printFlt(3);
+    printStr("\n\n");
 }
