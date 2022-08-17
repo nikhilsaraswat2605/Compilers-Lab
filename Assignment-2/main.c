171284442cb22ad895d544169363402ffdf78d10
Nikhil <nikhilsaraswat2605@gmail.com>
2022-08-17 22:08:42 +0530

    commited

--- Assignment-2/main.c
+++ Assignment-2/main.c
@@ -3,73 +3,88 @@
 #define INT_MAX __INT32_MAX__
 #define INT_MIN (-INT_MAX - 1)
 
-int main(){
-
-    printStr("#### Testing printStr ####\n");
-    char *s[3] = {"This is a sample string to be printed.", "Next follows an empty string.", ""};
+int main()
+{
+    printStr("\n----------------------------------------------------------------------------------------------\n");
+    printStr("#### Testing of printStr function ####\n");
+    printStr("----------------------------------------------------------------------------------------------\n");
+    char *s[] = {"I am 3rd year undergraduate student at IIT Kharagpur.", "My Department is Computer Science and Engineering.", "", "    "};
     char *newline = "\n";
-    for(int i=0; i<3; i++){
-        printStr("\""); 
-        int ret = printStr(s[i]); 
+    for (int i = 0; i < 4; i++)
+    {
+        printStr(newline);
+        printStr("\"");
+        int number_of_chars = printStr(s[i]);
         printStr("\" -> number of characters printed = ");
-        printInt(ret);
+        printInt(number_of_chars);
         printStr(newline);
     }
-    printStr(newline);
-
-    printStr("#### Testing printInt ####\n");
-    int ints[11] = {0, 1, -1, 12, -12, 1234, -1234, 12345, -12345, INT_MAX, INT_MIN};
-    for(int i=0; i<11; i++){
-        int ret = printInt(ints[i]);
+    printStr("\n----------------------------------------------------------------------------------------------\n");
+    printStr("#### Testing of printInt function ####\n");
+    printStr("----------------------------------------------------------------------------------------------\n");
+    int integers[] = {INT_MAX, INT_MIN, 11, 212, -323, -324324, 0, 3123, 332432, 43243, 989797, -432, -4234};
+    int n = sizeof(integers) / sizeof(int);
+    for (int i = 0; i < n; i++)
+    {
+        printStr(newline);
+        int number_of_chars = printInt(integers[i]);
         printStr(" -> number of characters printed = ");
-        printInt(ret);
+        printInt(number_of_chars);
         printStr(newline);
     }
-    printStr(newline);
-
-    printStr("#### Testing printFlt ####\n");
-    float floats[10] = {0.F, -12.34F, 12.345F, -2.F, 2.F, 0.1234F, -0.1234F, 123.456F, 0.001F, -0.0106F};
-    for(int i=0; i<10; i++){
-        int ret = printFlt(floats[i]);
+    printStr("\n----------------------------------------------------------------------------------------------\n");
+    printStr("#### Testing of printFlt function ####\n");
+    printStr("----------------------------------------------------------------------------------------------\n");
+    float floating_point_numbers[] = {0.242F, -32.232F, -0.232F, 0.F, 0232.232F, -0.F, 434.43F, .343F};
+    n = sizeof(floating_point_numbers) / sizeof(float);
+    for (int i = 0; i < n; i++)
+    {
+        printStr(newline);
+        int number_of_chars = printFlt(floating_point_numbers[i]);
         printStr(" -> number of characters printed = ");
-        printInt(ret);
+        printInt(number_of_chars);
         printStr(newline);
     }
-    printStr(newline);
-
-    printStr("#### Testing readInt ####\n");
-    int ntemp;
+    printStr("\n----------------------------------------------------------------------------------------------\n");
+    printStr("#### Testing of readInt function ####\n");
+    printStr("----------------------------------------------------------------------------------------------\n");
     int rep = 0;
-    do {
-        printStr("Enter an integer: ");
-        int ret = readInt(&ntemp);
-        if(ret == ERR) 
-            printStr("Invalid input. ");
-        else{
+    do
+    {
+        int number;
+        printStr("Enter an integer number: ");
+        if (readInt(&number) == OK)
+        {
             printStr("Successfuly read integer: ");
-            printInt(ntemp);
+            printInt(number);
         }
-        printStr("\nTo continue reading enter 1 otherwise 0: ");
+        else
+        {
+            printStr("Invalid input. Please try again.\n");
+        }
+        printStr("\nType 0 to exit otherwise 1 to continue reading integer number : ");
         readInt(&rep);
-    } while(rep != 0);
-    printStr(newline);
-
-    printStr("#### Testing readFlt ####\n");
-    float ftemp;
+    } while (rep != 0);
+    printStr("\n----------------------------------------------------------------------------------------------\n");
+    printStr("#### Testing of readFlt function ####\n");
+    printStr("----------------------------------------------------------------------------------------------\n");
     rep = 0;
-    do {
-        printStr("Enter a float: ");
-        int ret = readFlt(&ftemp);
-        if(ret == ERR) 
-            printStr("Invalid input. ");
-        else{
-            printStr("Successfuly read float: ");
-            printFlt(ftemp);
+    do
+    {
+        float number;
+        printStr("Enter a floating point number: ");
+        if (readFlt(&number) == OK)
+        {
+            printStr("Successfuly read floating point number: ");
+            printFlt(number);
         }
-        printStr("\nTo continue reading enter 1 otherwise 0: ");
+        else
+        {
+            printStr("Invalid input. Please try again.\n");
+        }
+        printStr("\nType 0 to exit otherwise 1 to continue reading floating point number : ");
         readInt(&rep);
-    } while(rep != 0);
-    printStr(newline);
+    } while (rep != 0);
 
     return 0;
 }
